7bca87cf97b360f494cd60e26d207a731fcdbf43
Franco <aguilarfranco1092@protonmail.com>
2022-11-19 22:05:11 -0600

    Starter code for thymeleaf views. Completed chapter walkthrough.

---
+++ src/main/java/org/launchcode/mycodingevents/MyCodingEventsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.mycodingevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodingEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCodingEventsApplication.class, args);
+	}
+
+}
