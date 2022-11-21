afff2a3c15d7f7e8f98f2543e0b57f4c7190b232
Franco <aguilarfranco1092@protonmail.com>
2022-11-21 16:42:39 -0600

    Models and model binding chapter walkthrough completed.

--- src/main/java/org/launchcode/mycodingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/mycodingevents/controllers/EventController.java
@@ -1,35 +1,57 @@
 package org.launchcode.mycodingevents.controllers;
 
+import org.launchcode.mycodingevents.data.EventData;
+import org.launchcode.mycodingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
+
         return "events/index";
     }
 
     @GetMapping("create")
-    public String displayCreateForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+
         return "events/create";
     }
 
     @PostMapping("create")
-    public String processCreateForm(@RequestParam String eventName, Model model) {
-        events.add(eventName);
+    // ModelAttribute lets us give spring the ability to create the Event object for us given the description values in the request
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+
+        return "redirect:";
+    }
+
+    // Controller method to get a view to delete events.
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Event");
+        model.addAttribute("events", EventData.getAll());
+
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    // (required = false) parameter of @RequestParam allows the user to submit the form without any events selected.
+    // Must also check the value is not null
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) {
+        if(eventIds != null) {
+            for(int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
         return "redirect:";
     }
 
---
+++ src/main/java/org/launchcode/mycodingevents/data/EventData.java
@@ -0,0 +1,36 @@
+package org.launchcode.mycodingevents.data;
+
+import org.launchcode.mycodingevents.models.Event;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventData {
+
+    // Java class to store and manipulate Event data
+    private static Map<Integer, Event> events = new HashMap<>();
+
+    // Why do we use Collection class?
+    public static Collection<Event> getAll() {
+        return events.values();
+    }
+
+    // Add an event
+    public static void add(Event event) {
+        events.put(event.getId(), event);
+    }
+
+    // Find event by id
+    public static Event getById(Integer id) {
+        return events.get(id);
+    }
+
+    // Remove event that is specified by its id
+    public static void remove(Integer id) {
+        if (events.containsKey(id)) {
+            events.remove(id);
+        }
+    }
+
+}
