f3da5d084937da6722d5e12d3bd6807119a4f028
Franco <aguilarfranco1092@protonmail.com>
2022-11-21 22:24:32 -0600

    Completed model validation walkthrough

--- src/main/java/org/launchcode/mycodingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/mycodingevents/controllers/EventController.java
@@ -2,9 +2,13 @@ package org.launchcode.mycodingevents.controllers;
 
 import org.launchcode.mycodingevents.data.EventData;
 import org.launchcode.mycodingevents.models.Event;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("events")
@@ -21,13 +25,23 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        // Pass in empty Event object because it has information on event fields to help render form more efficiently.
+        model.addAttribute(new Event());
 
         return "events/create";
     }
 
     @PostMapping("create")
     // ModelAttribute lets us give spring the ability to create the Event object for us given the description values in the request
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    // Spring Boot makes an object of type Errors available when a method uses @Valid.
+    // As with Model objects, we can access this object by placing it in a method’s parameter list.
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            // If there is validations errors, we will return the form again
+            return "events/create";
+        }
+
         EventData.add(newEvent);
 
         return "redirect:";
@@ -66,10 +80,11 @@ public class EventController {
     }
 
     @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description) {
+    public String processEditForm(int eventId, String name, String description, String contactEmail) {
         Event editedEvent = EventData.getById(eventId);
         editedEvent.setName(name);
         editedEvent.setDescription(description);
+        editedEvent.setContactEmail(contactEmail);
 
         return "redirect:";
     }
--- src/main/java/org/launchcode/mycodingevents/models/Event.java
+++ src/main/java/org/launchcode/mycodingevents/models/Event.java
@@ -1,19 +1,35 @@
 package org.launchcode.mycodingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+// Validation must take place in both the model AND controller.
 public class Event {
 
     private int id;
     private static int nextId = 1;
-
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank(message = "Name is required.")
     private String name;
+    @Size(max = 270, message = "Description too long.")
     private String description;
+    @Email(message = "Invalid email. Try again.")
+    @NotBlank(message = "Email is required.")
+    private String contactEmail;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+    }
+
+    public Event() {
         this.id = nextId;
         nextId++;
-    }
+    };
 
     public String getName() {
         return name;
@@ -31,6 +47,14 @@ public class Event {
         this.description = description;
     }
 
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
     public int getId() {
         return this.id;
     }
@@ -38,5 +62,18 @@ public class Event {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
